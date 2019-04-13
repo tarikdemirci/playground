aea85761ed73fc9c218f7337523134b8a2556c20
Tarik Demirci <tarikdemirci@users.noreply.github.com>
2019-04-13 20:07:25 +0200

    Implement dummy Percolation class

---
+++ algorithms/percolation/Percolation.java
@@ -0,0 +1,36 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Percolation {
+    // Create n-by-n grid. All sites are blocked initially.
+    public Percolation(int n) {
+
+    }
+
+    public void open(int row, int col) {
+
+    }
+
+    public boolean isOpen(int row, int col) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public boolean isFull(int row, int col) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public int numberOfOpenSites() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public boolean percolates() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

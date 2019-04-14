48d7e21c81b1af7ecb080c3cdab0aff95e6731ac
Tarik Demirci <tarikdemirci@users.noreply.github.com>
2019-04-14 19:54:02 +0200

    Implement simple methods

--- algorithms/percolation/Percolation.java
+++ algorithms/percolation/Percolation.java
@@ -1,36 +1,63 @@
-/* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
- **************************************************************************** */
-
 public class Percolation {
+    private static final byte BLOCKED_STATE = 0;
+    private static final byte OPEN_STATE = 1;
+
+    // Hold state for all sites.
+    private byte[][] grid;
+
+    private int openSiteCount;
+
+
+
     // Create n-by-n grid. All sites are blocked initially.
     public Percolation(int n) {
-
+        grid = new byte[n][n];
+        openSiteCount = 0;
     }
 
     public void open(int row, int col) {
+        if (! areRowAndColValid(row, col)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (grid[row - 1][col - 1] == BLOCKED_STATE) {
+            openSiteCount++;
+        }
+
+        grid[row - 1][col - 1] = OPEN_STATE;
 
     }
 
     public boolean isOpen(int row, int col) {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        if (! areRowAndColValid(row, col)) {
+            throw new IllegalArgumentException();
+        }
+
+        return grid[row - 1][col - 1] == OPEN_STATE;
     }
 
     public boolean isFull(int row, int col) {
+        if (! areRowAndColValid(row, col)) {
+            throw new IllegalArgumentException();
+        }
+
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     public int numberOfOpenSites() {
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return openSiteCount;
     }
 
     public boolean percolates() {
         throw new UnsupportedOperationException("Not implemented yet.");
     }
 
+    private boolean areRowAndColValid(int row, int col) {
+        return 1 <= row && row <= grid.length && 1 <= col && col <= grid[0].length;
+    }
+
     public static void main(String[] args) {
 
     }
+
 }
